5c470b7acd8b01e78cba39a52921a732e911afd5
Luisfelipedev27 <luisfelipe1582@hotmail.com>
2022-09-12 03:58:26 -0300

    Estilização de componentes + adição de Javascript com requisição Ajax (API CEP)

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_11_051157) do
+ActiveRecord::Schema.define(version: 2022_09_11_192026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "user_informations", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "name"
+    t.string "cpf", null: false
+    t.string "email", null: false
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_informations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +38,5 @@ ActiveRecord::Schema.define(version: 2022_09_11_051157) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_informations", "users"
 end
